7bb24f9c49663d3a0dd3b25d7eb345c6c279e67b
Bappaditya Banerjee <104856952+bappadityams@users.noreply.github.com>
2024-04-03 16:55:01 -0400

    update readme

--- 4-spring-web-app/1-Authentication/sign-in/src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapp/SecurityConfig.java
+++ 4-spring-web-app/1-Authentication/sign-in/src/main/java/com/microsoft/azuresamples/msal4j/msidentityspringbootwebapp/SecurityConfig.java
@@ -3,39 +3,34 @@
 
 package com.microsoft.azuresamples.msal4j.msidentityspringbootwebapp;
 
-import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadResourceServerHttpSecurityConfigurer;
 import com.azure.spring.cloud.autoconfigure.implementation.aad.security.AadWebApplicationHttpSecurityConfigurer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.lang.NonNull;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// This class is used to configure the security of the application.
+// To provide a configuration, apply the AadWebApplicationHttpSecurityConfigurer#aadWebApplication method for the HttpSecurity, as shown in the following example
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig  {
-
+    
     @Value("${app.protect.authenticated}")
     private String[] allowedOrigins;
-
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // @formatter:off
         http.apply(AadWebApplicationHttpSecurityConfigurer.aadWebApplication())
-               .and()
-           .authorizeHttpRequests()
-               .anyRequest().authenticated();
+        .and()
+        .authorizeHttpRequests()
+        .anyRequest().authenticated();
         // @formatter:on
         return http.build();
-    }
-
+    }    
 }
